a6379d03b5166c3a12d0998f3cea5ffa26b29d32
willbmisled <milstead.bryan@epa.gov>
2014-06-16 17:11:23 -0400

    convert csv to rda

--- DDPproject/server.R
+++ DDPproject/server.R
@@ -1,7 +1,7 @@
 library(shiny)
 
 #get the data
-mnka<-read.csv(url('https://raw.github.com/willbmisled/datasciencecoursera/master/DDPproject/FJmnka.csv'))
+mnka<-read.csv(url('https://raw.github.com/willbmisled/datasciencecoursera/master/DDPproject/mnka.rda'))
 closeAllConnections()#close connections to server
 
 #start Shiny app
